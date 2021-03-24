5a76829b040f2d13b825a5e70ee7b53e95b23e49
Oscar <askarrussia@gmail.com>
2021-03-24 09:27:29 +0500

    Настройки для деплоя

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
+const path = require('path')
 
 const PORT = config.get('port')
 const MONGO_URI = config.get('mongoUri')
 
 const app = express()
+
 app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/links.routes'))
 app.use('/t', require('./routes/redirect.routes'))
+
+if (process.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const startServer = async () => {
   try {
